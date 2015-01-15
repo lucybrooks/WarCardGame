f6a63567c76a14d86343728877a318b132c42c7a
lucybrooks <lucybrooks@users.noreply.github.com>
2015-01-15 08:15:52 -0700

    Update CardDeck.java

--- src/CardDeck.java
+++ src/CardDeck.java
@@ -1,1 +1,21 @@
-// create class CardDeck that extends CardList
+public class CardDeck extends CardList
+{
+  //feilds: suits, ranks, swaps
+  private int suits;
+  private int ranks;
+  private int swaps;
+  
+  //Constructor CardDeck()
+  public CardDeck()
+  {
+    suits=0;
+    ranks=0;
+    swaps=0;
+  }
+  
+  //Accessors shuffle
+  public int shuffle()
+  {
+    //next
+  }
+}
