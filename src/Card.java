ed9d04f8ae476a913820bcdc2c2a6f1944ae9fed
lucybrooks <lucybrooks@users.noreply.github.com>
2015-01-15 08:15:17 -0700

    Update Card.java

--- src/Card.java
+++ src/Card.java
@@ -1,2 +1,19 @@
-// Complete Class card by extending abstract Class PlayingCard and implementing the Comparable interface
-
+public class Card extends PlayingCard implements Comparable <Card>
+{
+  //constructor - Card(int suit, int rank)
+  public Card(int suit, int rank)
+  {
+    super(suit, rank);
+  }
+  
+  //Accessors - to String
+  public String toString()
+  {
+    return this.getRank()+"-"+this.getSuit();
+  }
+  // - compareTo(..): compare by rank
+  public int compareTo(Card other)
+  {
+    return this.getRank()-other.getRank();
+  }
+}
