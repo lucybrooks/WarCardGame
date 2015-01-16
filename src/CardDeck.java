b0b3dd576dfff74811da888abc29e5b7ac7913d2
lucybrooks <lucybrooks@users.noreply.github.com>
2015-01-16 08:20:32 -0700

    Update CardDeck.java

--- src/CardDeck.java
+++ src/CardDeck.java
@@ -3,19 +3,31 @@ public class CardDeck extends CardList
   //feilds: suits, ranks, swaps
   private int suits;
   private int ranks;
-  private int swaps;
+  private int swaps=10000;
   
   //Constructor CardDeck()
   public CardDeck()
   {
-    suits=0;
-    ranks=0;
-    swaps=0;
+    super();
+    suits=4;
+    ranks=13;
+    for(int suit =1; suit<suits; suit++)
+    {
+      for(int rank=1; rank<ranks; rank++)
+      {
+        addCardToBottom(new Card(suit, rank));
+      }
+    }
   }
   
   //Accessors shuffle
-  public int shuffle()
+  public void shuffle()
   {
-    //next
+    Card card;
+    for(int count=1; count<swaps; count++)
+    {
+      card=this.removeRandomCard();
+      this.addCardToBottom(card);
+    }
   }
 }
