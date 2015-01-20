26764872f1a7787603da4baf5790db4cf2a61210
lucybrooks <lucybrooks@users.noreply.github.com>
2015-01-19 21:27:51 -0700

    Update Card.java

--- src/Card.java
+++ src/Card.java
@@ -1,15 +1,35 @@
 public class Card extends PlayingCard implements Comparable <Card>
 {
-  //constructor - Card(int suit, int rank)
+ private String[] suitstring = new String[5];
+  private String[] rankstring = new String[14];
+  
   public Card(int suit, int rank)
   {
     super(suit, rank);
+    suitstring[1] = "H";
+    suitstring[2] = "C";
+    suitstring[3] = "D";
+    suitstring[4] = "S";
+    
+    rankstring[1] = "A";
+    rankstring[2] = "2";
+    rankstring[3] = "3";
+    rankstring[4] = "4";
+    rankstring[5] = "5";
+    rankstring[6] = "6";
+    rankstring[7] = "7";
+    rankstring[8] = "8";
+    rankstring[9] = "9";
+    rankstring[10] = "10";
+    rankstring[11] = "J";
+    rankstring[12] = "Q";
+    rankstring[13] = "K";
   }
   
   //Accessors - to String
   public String toString()
   {
-    return this.getRank()+"-"+this.getSuit();
+    return rankstring[this.getRank()]+"-"+suitstring[this.getSuit()];
   }
   // - compareTo(..): compare by rank
   public int compareTo(Card other)
